8e3f733b1102cf379c2f39616ed9a4f1b882db57
jay-n-prajapati <jay.n.prajapati@bacancy.com>
2024-04-07 09:41:23 +0530

    [FEAT] : library page added and modal added when deleting

--- client/src/components/common/DeleteButton.jsx
+++ client/src/components/common/DeleteButton.jsx
@@ -6,12 +6,27 @@ import { useDispatch } from 'react-redux';
 import { setAuth } from '@/redux/actions/authActions';
 import { deleteBlog, getUser, updateUser } from '@/utils/axios-instance';
 import { toast } from 'react-toastify';
+import {
+  Dialog,
+  DialogHeader,
+  DialogTrigger,
+  DialogTitle,
+  DialogDescription,
+  DialogContent,
+  DialogClose,
+} from '../ui/dialog';
+import { LoaderCircle } from 'lucide-react';
+import { useState } from 'react';
+import { setLoader } from '@/redux/actions/appActions';
+import { useSelector } from 'react-redux';
 
 const DeleteButton = ({ blog }) => {
   const { currentUser, endPoint, role } = useRole();
+  const { loader } = useSelector((state) => state.app);
   const dispatch = useDispatch();
 
   const handleDelete = async () => {
+    dispatch(setLoader(true));
     const filteredBlogs = currentUser.publishedBlogs.filter((blogId) => blogId !== blog.id);
     currentUser.publishedBlogs = filteredBlogs;
     const { error } = await updateUser(currentUser.id, endPoint, {
@@ -21,7 +36,7 @@ const DeleteButton = ({ blog }) => {
       toast.error(`Error : ${error}`);
       return;
     }
-    
+
     blog.savedBy.forEach(async (userId) => {
       const { data: userData, error: userErr } = await getUser(userId);
       if (userErr) {
@@ -29,12 +44,13 @@ const DeleteButton = ({ blog }) => {
         return;
       }
       const filteredSavedBlogs = userData.savedBlogs.filter((blogId) => blogId !== blog.id);
-      const { error: updateErr } = await updateUser(userId, 'users', {        // issue may be occur here
-        savedBlogs: filteredSavedBlogs,
-      });
-      if (updateErr) {
-        toast.error(`Error : ${updateErr}`);
-        return;
+      try {
+        await updateUser(userId, 'users', {
+          // issue may be occur here
+          savedBlogs: filteredSavedBlogs,
+        });
+      } catch (error) {
+        toast.error(`Error : ${error}`);
       }
     });
 
@@ -45,31 +61,57 @@ const DeleteButton = ({ blog }) => {
     }
     console.log('deleted');
     dispatch(setAuth(role, currentUser));
+    dispatch(setLoader(false));
   };
 
   return (
-    <CustomTooltip content='Delete Story'>
-      <Button variant='link' className={`px-0`} size='sm' onClick={handleDelete}>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width='22'
-          height='22'
-          viewBox='0 0 24 24'
-          fill='none'
-          stroke='black'
-          strokeWidth='1'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          className='lucide lucide-trash-2'
-        >
-          <path d='M3 6h18' />
-          <path d='M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6' />
-          <path d='M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2' />
-          <line x1='10' x2='10' y1='11' y2='17' />
-          <line x1='14' x2='14' y1='11' y2='17' />
-        </svg>
-      </Button>
-    </CustomTooltip>
+    <Dialog>
+      <DialogTrigger asChild>
+        {/* <CustomTooltip content='Delete Story'> */}
+        <Button variant='link' className={`px-0`} size='sm'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='22'
+            height='22'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='black'
+            strokeWidth='1'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            className='lucide lucide-trash-2'
+          >
+            <path d='M3 6h18' />
+            <path d='M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6' />
+            <path d='M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2' />
+            <line x1='10' x2='10' y1='11' y2='17' />
+            <line x1='14' x2='14' y1='11' y2='17' />
+          </svg>
+        </Button>
+        {/* </CustomTooltip> */}
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader className='my-2'>
+          <DialogTitle className='text-center text-3xl font-sohne-light'>
+            Are You Sure ?
+          </DialogTitle>
+          <DialogDescription className='text-center text-primary-text text-[14px]'>
+            Deleting this Content will permanently removed.
+          </DialogDescription>
+        </DialogHeader>
+        <div className='flex gap-4 items-center justify-center'>
+          <Button variant='destructive' onClick={handleDelete}>
+            {loader ? <LoaderCircle className='animate-spin mr-2' /> : null}
+            Delete
+          </Button>
+          <DialogClose asChild>
+            <Button type='button' variant='outline'>
+              Close
+            </Button>
+          </DialogClose>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 };
 
